ad5118c0a5d1dc0ebd2703cb2a3221bb1c7fdb47
OmarZamoranoGarcia <omarchucho13Zago0413@hotmail.com>
2025-10-05 15:45:27 -0700

    Interfaz de registro, pendiente el login y landingpage

---
+++ talentoSinBarreras/src/login/InputsLogin.jsx
@@ -0,0 +1,39 @@
+export function ImputsLogin({ label, placeholder, value, onChange, type = "text"}){
+    return(
+        <>
+            <label className="labelRegistro">{label}</label>
+            <input type={type} 
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+                className="inputRegistro"
+            />
+        </>
+    )
+}
+
+export function Select({ label}){
+    return(
+        <>
+            <label className="labelRegistro">{label}</label>
+            <select name="rolCuenta" id="selectRolCuenta">
+                <optgroup label="Instituciones">
+                    <option value="InstitucionPublica">Institución publica</option>
+                    <option value="InstitucionPrivada">Institución privada</option>
+                </optgroup>
+                <optgroup label="Postulantes">
+                    <option value="PersonaDiscapacitada">Persona discapacitada</option>
+                    <option value="PersonaServicioComunitario">Persona a realizar servicio comunitario</option>
+                    <option value="PersonaServicioSocial">Persona a realizar servicio social</option>
+                </optgroup>
+                
+            </select>
+        </>
+    )
+}
+
+export function Sumbit({text}){
+    return(
+        <button type="sumbit" id="btnSumbit">{text}</button>
+    )
+}
---
+++ talentoSinBarreras/src/login/Registro.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { ImputsLogin, Select, Sumbit } from "./InputsLogin"
+import "./InputsLogin.css";
+
+export function Registro(){
+    const [nombre, setNombre] = useState("");
+    const [apellidoPaterno, setApellidoPaterno] = useState("");
+    const [apellidoMaterno, setApellidoMaterno] = useState("");
+    const [correo, setCorreo] = useState("");
+    const [contrasenia, setContrasenia] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Datos de registro:", { nombre, email, password });
+        // aquí puedes llamar a tu backend o hacer validaciones
+    };
+    
+    return(
+    <form id="formRegistro" onSubmit={handleSubmit}>
+        <div id="contenedorTitulo">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svgUser">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+            <h1>Registro</h1>
+        </div>
+        
+        <div id="contenedorInputs">
+            <ImputsLogin
+                label="Nombre (s)"
+                placeholder="Nombre (s)"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+            />
+
+            <ImputsLogin
+                label="Apellido paterno"
+                placeholder="Apellido paterno"
+                value={apellidoPaterno}
+                onChange={(e) => setApellidoPaterno(e.target.value)}
+            />
+
+            <ImputsLogin
+                label="Apellido materno"
+                placeholder="Apellido materno"
+                value={apellidoMaterno}
+                onChange={(e) => setApellidoMaterno(e.target.value)}
+            />
+
+            <ImputsLogin
+                label="Correo"
+                placeholder="Correo"
+                value={correo}
+                onChange={(e) => setCorreo(e.target.value)}
+            />
+
+            <ImputsLogin
+                label="Contraseña"
+                placeholder="Contraseña"
+                value={contrasenia}
+                onChange={(e) => setContrasenia(e.target.value)}
+            />
+
+            <Select
+                label="Rol de la cuenta"
+            />
+
+            <Sumbit
+                text="Enviar Registro"
+            />
+        </div>
+
+    </form>
+  )
+}
